501358f38efa34e2fbf2fbf53a2e8c6fff2474db
Prabhu Ramamoorthy <140985234+PrabhuRamamoorthy@users.noreply.github.com>
2025-01-03 21:17:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,20 +12,16 @@ let data = {};
 app.post('/data', (req, res) => {
   const { id, bpm } = req.body;
   
-  // Check if required fields are present
   if (!id || bpm === undefined) {
     return res.status(400).json({ message: 'Invalid input. Please provide id and bpm.' });
   }
 
-  // Get the current time in UTC and convert to IST (India Standard Time)
-  const timestamp = moment().tz("Asia/Kolkata").format('DD-MM-YYYY HH:mm:ss');
+   const timestamp = moment().tz("Asia/Kolkata").format('DD-MM-YYYY HH:mm:ss');
 
-  // Initialize the data for this id if it doesn't exist
-  if (!data[id]) {
+   if (!data[id]) {
     data[id] = [];
   }
 
-  // Store the received data with the correct timestamp
   data[id].push({ id, timestamp, bpm });
 
   return res.status(201).json({ message: 'Data added successfully.', d: { id, timestamp, bpm } });
